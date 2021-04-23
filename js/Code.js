d1453defab04dbe097061f2ff53bae1cb00bed39
Omkar Kakkad <72384721+omkarkakkad@users.noreply.github.com>
2021-04-23 13:58:45 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(20);
+    text("R E V B A I L A", 50,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 50,70);
+// add code for displaying rest of the names and the hints.
+
+    textSize(20);
+    text("C U T N I F O N", 650,50);
+    fill("lightblue");
+    text("Hint: performs in particular tasks !!", 650,70);
+
+    fill("white");
+    textSize(20);
+    text("A T E D A S B A", 400,300);
+    fill("lightblue");
+    text("Hint: Stores all information !!", 400,320);
+}
